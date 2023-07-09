11ce2d6148e5abd48cd2d02bb2ef46ea4eec6834
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-09 15:03:47 +0300

    task-1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', () => {
+  changeBackgroundColor();
+  timerId = setInterval(changeBackgroundColor, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
