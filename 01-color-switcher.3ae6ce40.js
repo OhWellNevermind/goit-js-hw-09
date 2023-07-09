94035c813dd40c466ad49dcd25c6c132088b5049
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-09 12:06:41 +0000

    Deploying to gh-pages from @ OhWellNevermind/goit-js-hw-09@11ce2d6148e5abd48cd2d02bb2ef46ea4eec6834 🚀

---
+++ 01-color-switcher.3ae6ce40.js
@@ -0,0 +1,2 @@
+!function(){var t,n={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};function e(){n.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}n.stopBtn.disabled=!0,n.startBtn.addEventListener("click",(function(){e(),t=setInterval(e,1e3),n.startBtn.disabled=!0,n.stopBtn.disabled=!1})),n.stopBtn.addEventListener("click",(function(){clearInterval(t),n.startBtn.disabled=!1,n.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.3ae6ce40.js.map
